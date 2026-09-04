0ccac03398a4b1ed02402890afc824a212fa97f9
nolkhavatsal-afk <nolkhavatsal@gmail.com>
2026-09-05 00:13:40 +0530

    Add files via upload

---
+++ NUMBER GUESSING GAME.py
@@ -0,0 +1,16 @@
+import random
+number= random.randint(1,100)
+print('\n'*100)
+
+while True:
+    guess= int(input("Guess a number between 1 and 100: "))
+
+    if guess == number:
+        print("Congratulations! You guessed the correct number.")
+        break
+
+    elif guess < number:
+        print("Too low! Try again.")
+        
+    else:
+        print("Too high! Try again.")
